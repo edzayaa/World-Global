e8094b06d004dc3ddecd535228dafff963765e23
Criss <criss@faso.com.br>
2025-08-14 16:03:03 -0300

    Fix to build

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,6 @@ import { Section3 } from "./sections/section-3/section-3";
 import { Section4 } from "./sections/section-4/section-4";
 import { Section5 } from "./sections/section-5/section-5";
 import Section6 from "./sections/section-6/section-6";
-import { Section7 } from "./sections/section-7/section-7";
 import { Section2 } from "./sections/sections-2/section-2";
 
 export const dynamic = "force-static";
